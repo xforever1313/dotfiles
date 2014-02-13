a3b23ba107a71341cff23a18e7bcd0ea4fabbe57
Seth Hendrick <xforever1313@aim.com>
2014-02-13 15:31:52 -0500

    Install script now creates symlinks

--- install.sh
+++ install.sh
@@ -4,13 +4,32 @@ function makeDir(){
     fi
 }
 
-cp .bashrc ~/.bashrc
+function makeSymLink(){
+    if [ -e "$2" ] && [ ! -L "$2" ]; then
+        makeDir ~/dotfiles.old
+        mv $2 ~/dotfiles.old/`basename $2`
+    elif [ -L "$2" ]; then
+        rm $2
+    fi
+
+    ln -s $1 $2
+}
+
+function getCwd(){
+    if [ `uname` = "Linux" ]; then
+        cwd=`pwd`
+    else 
+        cwd=`cd`
+    fi
+}
 
 makeDir ~/.cache
 makeDir ~/.cache/vim
 makeDir ~/.cache/vim/backups
-makeDir ~/.vim
-makeDir ~/.vim/vimrc
 
-cp .vim/vimrc/* ~/.vim/vimrc
+getCwd
+
+makeSymLink $cwd/.bashrc ~/.bashrc
+makeSymLink $cwd/.vimrc ~/.vimrc
+makeSymLink $cwd/.vim ~/.vim
 
